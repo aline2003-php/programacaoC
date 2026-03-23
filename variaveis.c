e8bd8e0eb2560d773975bc145f0343dbf277dfd4
aline2003-php <202603539962@alunos.estacio.br>
2026-03-23 11:55:51 -0400

    Update variaveis.c

--- variaveis.c
+++ variaveis.c
@@ -1,12 +1,17 @@
 int main(){
-  int idade;        // inteiro
-  int idade;       // CASE SENSITIVE
-  int quantidade;
-  float altura;    // valores menores ex: 2,36
-  double altura;   // valores maiores depois da vírgula
-  char letra;      // armazena letras
-  char nome[20];     // array(caracteres minímos)
+  int idade;        // valor inteiro
+  int Idade = 10;       // CASE SENSITIVE
+  int IDADE = 10;       // CASE SENSITIVE
+  int quantidade = 1;
+  float altura = 1.75;    // valores menores ex: 2,36
+  double peso = 35.4;   // valores maiores depois da vírgula
+  char letra = 'A';      // armazena letras
+  char nome[20] = "Aline de Souza";     // array(caracteres minímos)
   int _teste;
   int T_2;
+
+  idade = 25;
+
+  quantidade = 10;
   
 }
