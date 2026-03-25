dbd58833c536e9bb16b218b68f4206a641688816
aline2003-php <202603539962@alunos.estacio.br>
2026-03-25 11:14:40 -0400

    Update variaveis2.c

--- variaveis2.c
+++ variaveis2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+  int idade = 23;                    %d
+  float altura = 1.30;               %e
+  char opcao = 'S';                  %c
+  char nome[20] = "aline de souza";  %s
+
+  printf("A idade da %s é: %d\n", nome, idade);
+  printf("A altura é: %e\n", altura);
+  printf("A opção é: %c\n", opcao);
+  
+  
+  
+  
+
+
+  
+}
